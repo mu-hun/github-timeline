51e01798742f0f6f448ebd235f13de6c74da565c
Muhun Kim <iam@muhun.kim>
2020-03-02 20:56:26 +0900

    Implement More load

---
+++ src/components/Button.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const More = styled.button`
+  width: 100%;
+  padding: 6px;
+  margin-top: 20px;
+  font-weight: 600;
+  color: #0366d6;
+  background: #fff;
+  background-color: rgb(255, 255, 255);
+  border: 1px solid #e1e4e8;
+  border-radius: 3px;
+
+  :hover {
+    color: #0366d6;
+    background-color: #f6f8fa;
+  }
+`;
