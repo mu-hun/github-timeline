b90c6aa5b44bd82cefbc1caa191ec8dd9e871b2c
Muhun Kim <iam@muhun.kim>
2020-03-02 15:10:01 +0900

    Hardcoded client id
    dotenv dosn't work

--- src/const.ts
+++ src/const.ts
@@ -1,6 +1,4 @@
-import 'dotenv/config';
-
-const clientID = process.env.clientID!;
+const clientID = 'd512f8f3eb78b82e7645';
 const redirectURL = 'http://localhost:3000/timeline';
 
 export const authLink = `https://github.com/login/oauth/authorize?client_id=${clientID}&redirect_uri=${redirectURL}`;
