97fd4654ec6589b34472f2b9f69dc3e984c8d0b5
Muhun Kim <iam@muhun.kim>
2020-03-03 21:34:37 +0900

    Increased the font size of the More button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -10,7 +10,7 @@ export const More = styled.button`
   background-color: rgb(255, 255, 255);
   border: 1px solid #e1e4e8;
   border-radius: 3px;
-
+  font-size: 1em;
   :hover {
     color: #0366d6;
     background-color: #f6f8fa;
