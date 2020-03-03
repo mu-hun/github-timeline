364f203efcf8e5e506270c12374046468d8949e9
Muhun Kim <iam@muhun.kim>
2020-03-03 21:56:17 +0900

    Add link default color to Black

---
+++ src/styles/color.ts
@@ -0,0 +1,2 @@
+export const blue = '#0366d6';
+export const black = '#000000';
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
+import { blue, black } from './color';
+
 export const MarginRight = styled.span`
   margin-right: 16px;
 `;
@@ -8,6 +10,11 @@ export const Flex = styled.div`
   display: flex;
 `;
 
-export const Bold = styled.span`
+export const BoldLink = styled.a`
   font-weight: 600;
+  text-decoration: none;
+  color: ${black};
+  :hover {
+    color: ${blue};
+  }
 `;
