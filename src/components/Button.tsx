eb26279dd5fc9e3eebb194eeb493066c0f01f5de
Muhun Kim <iam@muhun.kim>
2020-03-03 21:57:05 +0900

    Reduce blue color code in Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,18 +1,19 @@
 import styled from 'styled-components';
+import { blue } from '../styles/color';
 
 export const More = styled.button`
   width: 100%;
   padding: 6px;
   margin-top: 20px;
   font-weight: 600;
-  color: #0366d6;
+  color: ${blue};
   background: #fff;
   background-color: rgb(255, 255, 255);
   border: 1px solid #e1e4e8;
   border-radius: 3px;
   font-size: 1em;
   :hover {
-    color: #0366d6;
+    color: ${blue};
     background-color: #f6f8fa;
   }
 `;
