8dae44d293d08d4b83c8a5216b537f27a7e07d44
Muhun Kim <iam@muhun.kim>
2020-03-04 20:52:17 +0900

    Replace hardcoded clientID to env variable
    close #12

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,4 +1,6 @@
-const clientID = 'd512f8f3eb78b82e7645';
+import 'dotenv/config';
+
+const clientID = process.env.REACT_APP_CLIENT_ID!;
 const redirectURL = 'http://localhost:3000/timeline';
 
 export const authLink = `https://github.com/login/oauth/authorize?client_id=${clientID}&redirect_uri=${redirectURL}`;
