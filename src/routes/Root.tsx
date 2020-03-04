ca638b5edd840f281ca4428b9b23afaa55ddc988
Muhun Kim <iam@muhun.kim>
2020-03-04 14:42:48 +0900

    Check is have token by alias method

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -5,8 +5,8 @@ import { useStore } from '../store/contexts';
 import { Redirect } from 'react-router-dom';
 
 export default function Root() {
-  const { token } = useStore();
-  if (token.length) return <Redirect to="/timeline" />;
+  const { isHaveToken } = useStore();
+  if (isHaveToken()) return <Redirect to="/timeline" />;
   return (
     <div>
       <a href={authLink}>로그인하기</a>
